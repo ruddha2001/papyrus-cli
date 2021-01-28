693e18bb73b5cef063a1ce25295ae8a4496d7615
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-28 12:26:03 +0530

    :sparkles: Add file upload request

--- src/handlers/fileHandler.ts
+++ src/handlers/fileHandler.ts
@@ -1,8 +1,16 @@
-import { promises } from "fs";
+import axios from "axios";
+import { createReadStream } from "fs";
+import * as FormData from "form-data";
+import { constants } from "../constants";
 
 export const uploadHandler = async (path: string) => {
   try {
-    await promises.readFile(path);
+    let formData = new FormData();
+    formData.append("title", "sample");
+    formData.append("file", createReadStream(path));
+    await axios.post(constants.SERVER_URL + "/file/upload", formData, {
+      headers: formData.getHeaders(),
+    });
   } catch (error) {
     console.log(error.message);
   }
