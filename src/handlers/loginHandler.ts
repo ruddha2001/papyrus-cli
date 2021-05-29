00fbd99c53105ca91828f2f1da262eb568081a8a
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-05-29 18:58:47 +0530

    :recycle: Decouple handler from controller

--- src/handlers/loginHandler.ts
+++ src/handlers/loginHandler.ts
@@ -1,7 +1,9 @@
 import cli from "cli-ux";
+import { nanoid } from "nanoid";
 
 export const handleBrowserLogin = async () => {
   cli.action.start("Openning browser");
+  const clientId = nanoid(10);
   await cli.open("https://www.aniruddha.net");
   cli.action.stop();
   cli.action.start("Waiting for response from server");
